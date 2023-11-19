0096d7771dd4ecdf420dc3b13fc8b4551f7100ab
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-11-19 14:09:08 +0700

    [IMPROVE] change name to getByIdAdmin

--- src/main/java/com/enigma/duitku/service/AdminService.java
+++ src/main/java/com/enigma/duitku/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.enigma.duitku.service;
 
 import com.enigma.duitku.entity.Admin;
+import com.enigma.duitku.entity.User;
 import com.enigma.duitku.exception.UserException;
 import com.enigma.duitku.model.response.UserResponse;
 import org.springframework.data.domain.Page;
@@ -8,5 +9,6 @@ import org.springframework.data.domain.Page;
 public interface AdminService {
 
     Admin create (Admin admin) throws UserException;
+    Admin getByIdAdmin(String id);
     Page<UserResponse> getAll(Integer page, Integer size);
 }
